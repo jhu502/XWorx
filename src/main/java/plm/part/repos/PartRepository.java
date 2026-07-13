2b85fcbad12bcefcae9ea58824983272d64d2b50
hujin <jhu502@sina.com>
2026-07-13 11:13:56 +0800

    feat:开发XDocument的相关功能

--- src/main/java/plm/part/repos/PartRepository.java
+++ src/main/java/plm/part/repos/PartRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface PartRepository extends JpaRepository<XPart, Long> {
-    @Query(value = "select a, b from XDocumentUsageLink a, XPart b where a.left.id = :#{#part.xid} and a.right.id = b.master.xid")
+    @Query(value = "select a, b from XPartUsageLink a, XPart b where a.left.id = :#{#part.xid} and a.right.id = b.master.xid")
     List<?> getUsedbyXPart(XPart part);
 
     @Query(value = "select p from XPart p where p.master.number like :keyword or p.master.name like :keyword")
